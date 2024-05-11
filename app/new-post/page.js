5957af1c63601a515d5f3f6f76bb4064a60164ae
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-11 23:02:43 +0300

    chore: Refactor new post page to use FormPost component and add form validation

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,46 +1,45 @@
-import { storePost } from '@/lib/posts';
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+import FormPost from "@/components/form-post";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
-    "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
+    async function createPost(prevState, formData) {
+        "use server";
+        const title = formData.get("title");
+        const image = formData.get("image");
+        const content = formData.get("content");
 
-    storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    })
-  }
+        let errors = [];
 
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
-        </p>
-      </form>
-    </>
-  );
+        if (!title || title.trim().length === 0) {
+            errors.push("Title cannot be empty.");
+        }
+
+        if (!content || content.trim().length === 0) {
+            errors.push("Content cannot be empty.");
+        }
+
+        if (!image) {
+            errors.push("Image is required.");
+        }
+
+        if (errors.length > 0) {
+            return { errors };
+        }
+
+        storePost({
+            imageUrl: "",
+            title,
+            content,
+            userId: 1,
+        });
+
+        redirect("/feed");
+    }
+
+    return (
+        <>
+            <FormPost action={createPost} />
+        </>
+    );
 }
