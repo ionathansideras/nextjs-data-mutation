1e141110cd8ab5f9e3b12cc7636e5da052d77c81
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-11 23:21:13 +0300

    chore: Refactor new post page to use FormPost component and add form validation

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,42 +1,7 @@
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
 import FormPost from "@/components/form-post";
+import { createPost } from "@/actions/post";
 
 export default function NewPostPage() {
-    async function createPost(prevState, formData) {
-        "use server";
-        const title = formData.get("title");
-        const image = formData.get("image");
-        const content = formData.get("content");
-
-        let errors = [];
-
-        if (!title || title.trim().length === 0) {
-            errors.push("Title cannot be empty.");
-        }
-
-        if (!content || content.trim().length === 0) {
-            errors.push("Content cannot be empty.");
-        }
-
-        if (!image) {
-            errors.push("Image is required.");
-        }
-
-        if (errors.length > 0) {
-            return { errors };
-        }
-
-        storePost({
-            imageUrl: "",
-            title,
-            content,
-            userId: 1,
-        });
-
-        redirect("/feed");
-    }
-
     return (
         <>
             <FormPost action={createPost} />
