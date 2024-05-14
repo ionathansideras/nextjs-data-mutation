7a21ae285443e4b2880b4d8b30c9c4ff4363e73a
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-14 11:49:07 +0300

    Refactor Posts component to use useOptimistic hook for managing post state

--- components/posts.js
+++ components/posts.js
@@ -1,9 +1,11 @@
+"use client";
 import { formatDate } from "@/lib/format";
+
 import LikeButton from "./like-icon";
 import { togglePostLikeStatus } from "@/actions/post";
-import { act, useOptimistic } from "react";
+import { useOptimistic } from "react";
 
-function Post({ post }) {
+function Post({ post, action }) {
     return (
         <article className="post">
             <div className="post-image">
@@ -35,39 +37,65 @@ function Post({ post }) {
     );
 }
 
-export default function Posts({ posts, action }) {
+export default function Posts({ posts }) {
+    // The `useOptimistic` hook is used to manage the state of the posts.
+    // It takes two arguments: the initial state (`posts`) and an update function.
+
+    // The initial state (`posts`) is an array of post objects.
+
+    // The update function takes the current state (`posts`) and an `updatePostId` as arguments.
+    // It returns a new state based on the current state and the `updatePostId`.
+
+    // The `useOptimistic` hook returns an array with two elements: the current state and a function to update the state.
+    // These are destructured into `optimisticPosts` and `updateOptimisticPosts`, respectively.
     const [optimisticPosts, updateOptimisticPosts] = useOptimistic(
         posts,
         (posts, updatePostId) => {
+            // Find the index of the post with the `updatePostId`.
+            // If no such post is found, `findIndex` returns -1.
             const updatedPostIndex = posts.findIndex(
                 (post) => post.id === updatePostId
             );
 
+            // If no post with the `updatePostId` is found, return the current state without any changes.
             if (updatedPostIndex === -1) {
                 return posts;
             }
 
+            // Create a copy of the post to be updated (the one we clicked).
             const updatedPosts = { ...posts[updatedPostIndex] };
+
+            // Update the `likes` property of the post.
+            // If the post is liked, decrement the `likes` by -1.
+            // If the post is not liked, increment the `likes` by 1.
             updatedPosts.likes =
                 updatedPosts.likes + (updatedPosts.isLiked ? -1 : 1);
 
+            // Toggle the `isLiked` property of the post.
+            // so if the post is liked, set it to `false`, and vice versa.
             updatedPosts.isLiked = !updatedPosts.isLiked;
 
+            // Create a copy of the current state.
             const newPosts = [...posts];
+
+            // Replace the post to be updated with the updated post in the new state.
             newPosts[updatedPostIndex] = updatedPosts;
 
+            // Return the new state.
             return newPosts;
         }
     );
+
     if (!optimisticPosts || optimisticPosts.length === 0) {
         return <p>There are no posts yet. Maybe start sharing some?</p>;
     }
 
     async function updatePost(postId) {
         updateOptimisticPosts(postId);
-
+        // Call the `togglePostLikeStatus` function to update the post's like status.
         await togglePostLikeStatus(postId);
     }
+
     return (
         <ul className="posts">
             {optimisticPosts.map((post) => (
